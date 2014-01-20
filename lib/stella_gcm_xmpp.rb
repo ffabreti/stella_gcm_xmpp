dcabbc1baf59e7d2984da7d2950fbebc752a2c3e
강훈 <gosomi@forelf.com>
2014-01-21 01:59:00 +0900

    New Project

---
+++ lib/stella_gcm_xmpp.rb
@@ -0,0 +1,66 @@
+require 'xmpp4r/client'
+require 'active_support/json'
+require 'active_support/core_ext/hash'
+
+class StellaGcmXmpp
+  def initialize(id, password, debug = false)
+    @id = id
+    @password = password
+    Jabber::debug = debug
+  end
+  def connect
+    jid = Jabber::JID::new(@id)
+
+    @client = Jabber::Client::new(jid)
+    @client.use_ssl = true
+    @client.connect("gcm.googleapis.com", 5235)
+    @client.auth(@password)
+  end
+  def callback(function = nil)
+    @client.add_message_callback do |m|
+      begin
+        result = Hash.from_xml(m.to_s).with_indifferent_access
+      rescue
+        return
+      end
+      begin
+        data = JSON.parse(result[:message][:gcm]).with_indifferent_access
+      rescue
+        return self.fail
+      end
+      if data[:message_type] == 'ack'
+        print "GCM send Success id: #{data[:message_id]}\n"
+      else
+        print "GCM send Failed id: #{data[:message_id]}\n"
+      end
+      call(function) unless function.nil?
+    end
+  end
+  def fail
+    print "GCM send Critical Error\n"
+  end
+  def send(to, message_id, data)
+    json = {:to => to,
+            :message_id => message_id,
+            :data => data,
+            :time_to_live => 600,
+            :delay_while_idle => false}
+    msg = "<message id=\"\">
+             <gcm xmlns=\"google:mobile:data\">
+               #{json.to_json}
+             </gcm>
+            </message>"
+    @client.send msg
+  end
+  def disconnect
+    @client.close
+    @client = nil
+  end
+  def reconnect
+    self.reconnect
+    self.connect
+  end
+  def stable_blank
+    self.send("NULL", "7573", {:type => nil})
+  end
+end
---
+++ stella_gcm_xmpp.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+  s.name = 'stella_gcm_xmpp'
+  s.version = '0.0.1beta'
+  s.date = '2014-01-21'
+  s.summary = ''
+  s.description = ''
+  s.authors = ['Haru']
+  s.email = 'aqure84@naver.com'
+  s.files = ["lib/stella_gcm_xmpp.rb"]
+  s.homepage = 'http://www.forelf.com'
+  s.add_dependency("xmpp4r")
+  s.required_ruby_version     = ">= 1.9"
+  s.required_rubygems_version = ">= 1.3.6"
+  s.rubyforge_project         = "stella_gcm_xmpp"
+end
